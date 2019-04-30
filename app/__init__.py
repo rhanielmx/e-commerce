2812d9354f74e3f3ad881cdcdadd8bd8c16f55e2
Rhaniel Magalhães <rhanielmag.xavier@gmail.com>
2019-04-30 14:55:28 -0300

    Lidando com requisição de login

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
-from flask import Flask
+from flask import Flask, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from flask_login import LoginManager
+
+from datetime import timedelta
+
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -15,6 +18,16 @@ manager.add_command('db', MigrateCommand)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
+login_manager.login_view = 'login'
+login_manager.refresh_view = 'login'
+login_manager.needs_refresh_message = (u"Session timedout, please re-login")
+login_manager.needs_refresh_message_category = "info"
+
+@app.before_request
+def before_request():
+    session.permanent = True
+    app.permanent_session_lifetime = timedelta(hours=1)
+
 
 from app.models import tables, forms
 from app.controllers import controller
